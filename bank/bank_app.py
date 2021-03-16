7ee7ccf69a8c3e9a4b330aca5e8cfa4bc007342c
Michaela Gasteratou <elagasteratou@gmail.com>
2021-03-16 13:54:08 +0000

    Added an else statement to our bank_app.py file

--- bank/bank_app.py
+++ bank/bank_app.py
@@ -4,13 +4,15 @@ from exceptions import InsufficientFunds
 
 # instantiate a bank account object
 bara = Account("bara", 500, 3)
-withdraw_amount = 600
+withdraw_amount = 60
 
 try:
     bara.make_withdrawal(withdraw_amount)
-    print(f"You have made a withdrawal {withdraw_amount}")
+    # print(f"You have made a withdrawal {withdraw_amount}")
 except InsufficientFunds:
     print("You have insufficient funds, please try again", file=sys.stderr)
+else:
+    print(f"You have made a withdrawal {withdraw_amount}")
 finally:
     print(f"Your current balance is {bara.get_balance()}")
 
